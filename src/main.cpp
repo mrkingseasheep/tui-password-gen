e9d25683ff749167c46a8fedde6516234c8c57aa
Xinyang Wang <mrxywang@gmail.com>
2024-08-06 10:42:17 -0400

    Renderer added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
-#include "ftxui/component/captured_mouse.hpp" // for ftxui
 #include "ftxui/component/component.hpp" // for Checkbox, Renderer, Vertical
 #include "ftxui/component/component_base.hpp"     // for ComponentBase
 #include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
 #include "ftxui/dom/elements.hpp" // for operator|, Element, size, border, frame, vscroll_indicator, HEIGHT, LESS_THAN
 #include "ftxui/dom/node.hpp"     // for Render
 #include "ftxui/screen/color.hpp" // for ftxui
-#include <algorithm>
 #include <ftxui/component/component_options.hpp>
 #include <ftxui/dom/elements.hpp> // for operator|, size, Element, text, hcenter, Decorator, Fit, WIDTH, hflow, window, EQUAL, GREATER_THAN, HEIGHT, bold, border, dim, LESS_THAN
 #include <ftxui/screen/screen.hpp> // for Full, Screen
-#include <iostream>
-#include <stdio.h> // for getchar
-#include <string>  // for allocator, char_traits, operator+, to_string, string
-#include <vector>
+#include <string> // for allocator, char_traits, operator+, to_string, string
 
 int main() {
     using namespace ftxui;
@@ -26,6 +21,11 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         container->Add(Checkbox(&options[i], &used[i]));
     }
+    auto renderer = Renderer(container, [&] {
+        return vbox({
+            hbox(text("Options"), container->Render()),
+        });
+    });
 
     screen.Loop(container);
 }
